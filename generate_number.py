0b79beeb743cb81210158f3523c379eb2e0a72d1
Kernel <kamran.nasim.96@gmail.com>
2023-06-17 16:06:41 +0500

    Update generate_number.py

--- generate_number.py
+++ generate_number.py
@@ -3,16 +3,14 @@
 numbers = []
 
 for i in range(10):
-    #if i not in [6, 7, 8, 9]:
+    if i not in [6, 7, 8, 9]:
         for j in range(10):
             for k in range(10):
                 for l in range(10):
                     for m in range(10):
                         for n in range(10):
                             for o in range(10):
-                                for p in range(10):
-                                    for q in range(10):
-                                number = f'03{i}{j}{k}{l}{m}{n}{o}{p}{q}' #Change 03 with country code
+                                number = f'+923{i}{j}{k}{l}{m}{n}{o}23'  #Change +923 with country code and if you want to add last digits you can do that or remove 23
                                 numbers.append(number)
 
 # Save the generated numbers in a text file
