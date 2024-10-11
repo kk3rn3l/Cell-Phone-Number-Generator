6e2874f132dd99454b22be1a208f9e7b13c7cf89
kk3rn3l <kamran.nasim.96@gmail.com>
2024-10-11 22:49:18 +0500

    Initial commit

--- generate_number.py
+++ generate_number.py
@@ -1,17 +1,20 @@
-#!/bin/python
+#!/usr/bin/python
 
 numbers = []
 
 for i in range(10):
-    if i not in [6, 7, 8, 9]: #replace any digit or comment out this section if you want 6,7,8,9 in the place of {i} 
-        for j in range(10):
-            for k in range(10):
-                for l in range(10):
-                    for m in range(10):
-                        for n in range(10):
-                            for o in range(10):
-                                number = f'+923{i}{j}{k}{l}{m}{n}{o}23'  #Change +923 with country code and if you want to add last digits you can do that or remove 23
-                                numbers.append(number)
+    # Uncomment the following line if you want to exclude 0, 1, 2, 3, 4, 6, 7, 8, 9
+    # if i not in [0, 1, 2, 3, 4, 5]:  # This will exclude 6, 7, 8, 9 if desired
+    for j in range(10):
+        for k in range(10):
+            for l in range(10):
+                for m in range(10):
+                    for n in range(10):
+                        for o in range(10):
+                            for p in range(10):
+                                for q in range(10):
+                                    number = f'+923{i}{j}{k}{l}{m}{n}{o}{p}{q}'  # Change +923 to the desired country code
+                                    numbers.append(number)
 
 # Save the generated numbers in a text file
 with open('output.txt', 'w') as file:
